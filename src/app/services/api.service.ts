695c22eb55a92719cd63d3a95c83352255e8c2ea
Oscar Zambrano <OscarEZP@users.noreply.github.com>
2025-06-09 10:49:46 +0200

    feat: add environment and api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getMachines(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/machines`);
+  }
+}
